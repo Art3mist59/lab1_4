cb23f3664dc917ffd3e6a9a119670c49bdfd31bc
Art3mist59 <Stryapunin9@gmail.com>
2025-01-16 20:38:59 +0500

    1

--- lib.py
+++ lib.py
@@ -11,15 +11,3 @@ def count_common_elements(*lists):
         common_elements.intersection_update(lst)
 
     return len(common_elements)
-
-if __name__ == "__main__":
-    # Пример использования функции
-    n = int(input("Введите количество списков: "))
-    lists = []
-
-    for i in range(n):
-        lst = input(f"Введите элементы списка {i + 1} через пробел: ").split()
-        lists.append(lst)
-
-    result = count_common_elements(*lists)
-    print(f"Количество одинаковых элементов в списках: {result}")
